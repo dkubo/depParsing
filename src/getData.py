d4fbb4daefe7b1c94330b613b1bcd7fe16e8d793
magnesium <daiki.kosen.10@gmail.com>
2016-06-09 19:39:12 +0900

    make code

--- src/getData.py
+++ src/getData.py
@@ -12,7 +12,7 @@ import re
 import codecs
 
 #######################################
-#				再帰オープン
+#		再帰オープン
 #######################################
 def fild_all_files(directory):
 	for root, dirs, files in os.walk(directory):
@@ -23,15 +23,36 @@ def fild_all_files(directory):
 
 
 if __name__ == '__main__':
-	for file in fild_all_files(ROOT_DIR):
-		matchOB=re.search(r'KNP$',file)
-		if matchOB:
-			with codecs.open(file,'r','euc_jp') as f:
-				for line in f:
-					arr=line.split()
-#					print(arr)
-					if arr[0] == '*':
-						print(line)
+	sentID=1		#文に割り当てるid
+	bunsetsu=""
+	bunsetsuArr=[]
+	sentenceHash={}
+#	for file in fild_all_files(ROOT_DIR):		#ファイル再帰オープン
+	file=ROOT_DIR+'950101.KNP'							#まずは1月1日のみを対象とする
+	matchOB=re.search(r'KNP$',file)
+	if matchOB:
+		print("---------------------------------------")
+		print(file)
+		print("---------------------------------------")
+		with codecs.open(file,'r','euc_jp') as f:
+			for line in f:
+				arr=line.split()
+				if arr[0] == '#':
+					continue
+				elif arr[0] == '*':
+					bunsetsuID = arr[1]
+					if bunsetsu != '':
+						bunsetsuArr.append(bunsetsu)
+					bunsetsu = ""
+				elif arr[0] == 'EOS':
+					print(sentID)
+					sentenceHash[sentID]=bunsetsuArr
+					bunsetsuArr=[]
+					sentID+=1
+
+				else:
+					bunsetsu+=arr[0]
+#			print(sentenceHash)
 
 
 
