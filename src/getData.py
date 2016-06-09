4a7265b3c2a2aadcffc3c9320f1d29c8792df795
magnesium <daiki.kosen.10@gmail.com>
2016-06-09 18:42:45 +0900

    make code

--- src/getData.py
+++ src/getData.py
@@ -1,19 +1,19 @@
 #coding:utf-8
 
 """
-KyotoCorpus4.0-200707/dat/synから、素性取得
+/KyotoCorpus4.0-200707/dat/synから素性取得
 2016/6/9 @dkubo
 """
 
-ROOT_DIR="../data/KyotoCorpus4.0-200707/dat/syn/"
+ROOT_DIR="/home/daiki/デスクトップ/naist/class/ProjectP/data/KyotoCorpus4.0-200707/dat/syn/"
 
 import os
 import re
 import codecs
 
-#######################################3
+#######################################
 #				再帰オープン
-#######################################3
+#######################################
 def fild_all_files(directory):
 	for root, dirs, files in os.walk(directory):
 		yield root
@@ -24,12 +24,14 @@ def fild_all_files(directory):
 
 if __name__ == '__main__':
 	for file in fild_all_files(ROOT_DIR):
-		matchOB=re.search(r"KNP$",file)
+		matchOB=re.search(r'KNP$',file)
 		if matchOB:
 			with codecs.open(file,'r','euc_jp') as f:
 				for line in f:
 					arr=line.split()
-					print(arr)
+#					print(arr)
+					if arr[0] == '#':
+						print(line)
 
 
 
